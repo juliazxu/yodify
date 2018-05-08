7f06f573336fc5bfcd6dba6a3f92b2e4834047cc
juliazxu <juliazxu@gmail.com>
2018-05-07 21:44:02 -0700

    Edited server.js and Procfile for heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var path = require('path');
+var express = require('express');
 
-app.use(express.static(__dirname + '/dist/'));
-app.use('/src/assets', express.static(__dirname + '/src/assets/'));
+var app = express();
 
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
 
-app.listen(process.env.PORT || 3843);
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
